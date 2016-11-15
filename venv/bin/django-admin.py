848956b3a7ac9aa21215e16a238e33870c17c1a9
Eric Scheier <eric@scheier.org>
2016-11-15 19:28:42 +0200

    setup project structure, .gitignore, requirements.txt

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/eric/Documents/GitHub/tradecultivation/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ venv/lib/python3.5/sre_compile.py
@@ -0,0 +1,1 @@
+/home/eric/anaconda3/lib/python3.5/sre_compile.py
